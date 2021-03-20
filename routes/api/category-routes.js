8b46ce8477edc45b88a4d8f5996aec201edffd94
George Cope <G.Cope13@gmail.com>
2021-03-20 19:09:30 +0000

    Created route update a product categorie via its ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -50,8 +50,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try{
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if(!categoryData[0]){
+      res.status(404).json({message: 'This category ID does not exist.'});
+      return;
+    }
+    res.json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
