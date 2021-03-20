24a9cff7a8a75a18213a6bbc755429eed1524382
George Cope <G.Cope13@gmail.com>
2021-03-20 19:36:17 +0000

    Created route to get product via its ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,9 +26,31 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{
+          model: Tag,
+          through: ProductTag,
+          as: 'tagged_products',
+        },
+        {
+          model: Category
+        }
+      ]
+    });
+
+    if(!productData){
+      res.status(404).json({message: 'This tag ID does not exist.'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
