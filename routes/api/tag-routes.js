8b575ebcd4598defaad62c0673f51af8076fea55
George Cope <G.Cope13@gmail.com>
2021-03-21 13:04:45 +0000

    Created route for getting a tag via its ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,9 +22,29 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const tagData = Tag.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          through: ProductTag,
+          as: 'selected_tags'
+        }
+      ]
+    });
+
+    if(!tagData){
+      res.status(404).json({message: 'This tag ID does not exist.'});
+      return:
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
